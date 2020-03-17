d237046b39b4ddd6241e22ff5cbaf52e8bb83fe4
Swati Joshi <swatijoshi@outlook.de>
2020-03-17 23:49:29 +0100

    modifide show methode to render show twig file

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * Undocumented function
@@ -13,12 +14,26 @@ class ArticleController
      */
     public function homepage()
     {
-        return new Response("my front page content");
+        return new Response('my first front page content');
     }
 
-    public function show()
+    /**
+     * @Route("news/{slug}")
+     */
+
+    public function show($slug)
     {
-        
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+        return $this->render('article/show.html.twig',[
+
+            'title'=> ucwords(str_replace('-',' ',$slug)),
+            'comments'=>$comments
+
+        ]);
     }
 
 }
