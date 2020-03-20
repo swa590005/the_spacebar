ddc8b04e6747325dbe7a020a31f3de3a647a356a
Swati Joshi <swatijoshi@outlook.de>
2020-03-20 11:36:42 +0100

    added cache service

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Michelf\MarkdownInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -25,7 +26,7 @@ class ArticleController extends AbstractController
      * @Route("news/{slug}", name="article_show")
      */
 
-    public function show($slug, MarkdownInterface $markdown)
+    public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache)
     {
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
@@ -37,7 +38,7 @@ class ArticleController extends AbstractController
         Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
         lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
         labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
-        turkey shank eu pork belly meatball non cupim.
+        **turkey** shank eu pork belly meatball non cupim.
         Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
         laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
         capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
@@ -50,7 +51,13 @@ class ArticleController extends AbstractController
         fugiat.    
         EOF;
 
-        $articleContent=$markdown->transform($articleContent);
+        $item=$cache->getItem('markdown_'.md5($articleContent));
+        if(!$item->isHit()){
+            $item->set($markdown->transform($articleContent));
+            $cache->save($item);
+        }
+
+        $articleContent=$item->get();
 
         return $this->render('article/show.html.twig',[
 
