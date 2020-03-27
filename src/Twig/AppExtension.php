8b7d4b45536969c82ad2c4dd89188ac1f2b17241
Swati Joshi <swatijoshi@outlook.de>
2020-03-27 12:04:56 +0100

    created lazy loaded twig extension to avoid performance hit

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,31 +2,22 @@
 
 namespace App\Twig;
 
-use App\Service\MarkdownHelper;
+
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
-use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
 {
-    private $helper;
-    public function __construct( MarkdownHelper $helper  )
-    {
-        $this->helper=$helper;
-    }
-
+   
     public function getFilters(): array
     {
         return [
             // If your filter generates SAFE HTML, you should add a third
             // parameter: ['is_safe' => ['html']]
             // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
-            new TwigFilter('cached_markdown', [$this, 'processMarkdown'],['is_safe' => ['html']]),
+            new TwigFilter('cached_markdown', [AppRuntime::class, 'processMarkdown'],['is_safe' => ['html']]),
         ];
     }
    
-    public function processMarkdown($value)
-    {
-        return $this->helper->parse($value);
-    }
+    
 }
