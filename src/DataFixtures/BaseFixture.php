3fe25f4898c060fed36dc1f7548e63ec47190327
Aditya Joshi <adi3230@gmail.com>
2020-03-31 16:36:52 +0200

    faker factory create

--- src/DataFixtures/BaseFixture.php
+++ src/DataFixtures/BaseFixture.php
@@ -4,18 +4,25 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 abstract class BaseFixture extends Fixture
 {
     /** @var ObjectManager */
-    
+
     private $manager;
+    /**
+     * Undocumented variable
+     *
+     * @var Generator
+     */
+    protected $faker;
     abstract protected function loadData(ObjectManager $em );
    
     public function load(ObjectManager $manager)
     {
         $this->manager= $manager;
-
+        $this->faker= Factory::create();
         $this->loadData($manager);
     }
     protected function createMany(string $className, int $count, callable $factory)
