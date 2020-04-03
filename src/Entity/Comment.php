78f61a39c9ebc1f3e6c0fbcb1775d3a0282e2083
Swati Joshi <swatijoshi@outlook.de>
2020-04-03 16:44:56 +0200

    adding Manytoone in comment and onetomany relation in article using entity

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -26,6 +26,12 @@ class Comment
      */
     private $content;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Article", inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $article;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -51,6 +57,18 @@ class Comment
     public function setContent(string $content): self
     {
         $this->content = $content;
+
+        return $this;
+    }
+
+    public function getArticle(): ?Article
+    {
+        return $this->article;
+    }
+
+    public function setArticle(?Article $article): self
+    {
+        $this->article = $article;
 
         return $this;
     }
