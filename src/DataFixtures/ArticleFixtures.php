2d12666ac08410f6b4c730de63f94aba6e73ee05
Swati Joshi <swatijoshi@outlook.de>
2020-04-03 16:57:34 +0200

    saving relations meaning saving comment for each article using fixtures

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Comment;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class ArticleFixtures extends BaseFixture
@@ -24,7 +25,7 @@ class ArticleFixtures extends BaseFixture
     protected function loadData(ObjectManager $manager)
     {
 
-        $this->createMany(Article::class,10, function(Article $article, $count )
+        $this->createMany(Article::class,10, function(Article $article, $count ) use ($manager)
         {
            
             $article->setTitle($this->faker->randomElement(self::$articleTitles))
@@ -55,6 +56,19 @@ class ArticleFixtures extends BaseFixture
                 $article->setAuthor($this->faker->randomElement(self::$articleAuthors));
                 $article->setHeartCount($this->faker->numberBetween(5, 100));
                 $article->setImageFilename($this->faker->randomElement(self::$articleImages));
+
+                $comment1=new Comment();
+                $comment1->setAuthorName('Mike Ferengi');
+                $comment1->setContent('I ate a normal rock once. It did NOT taste like bacon!');
+                $comment1->setArticle($article);
+                $manager->persist($comment1);
+
+                $comment2 = new Comment();
+                $comment2->setAuthorName('Mike Ferengi');
+                $comment2->setContent('Woohoo! I\'m going on an all-asteroid diet!');
+                $comment2->setArticle($article);
+                $manager->persist($comment2);
+
         });
 
         $manager->flush();
