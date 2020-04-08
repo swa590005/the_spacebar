f2ec3d192182523aae953c8196f852158f9e1e14
Swati Joshi <swatijoshi@outlook.de>
2020-04-08 11:57:38 +0200

    added criteria code to show only non deleted comment

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Article;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\ORM\QueryBuilder;
 
@@ -34,6 +35,14 @@ class ArticleRepository extends ServiceEntityRepository
         ;
     }
 
+    public static function createNonDeletedCriteria()
+    {
+        return Criteria::create()
+        ->andWhere(Criteria::expr()->eq('isDeleted',false))
+        ->orderBy(['createdAt'=>'DESC'])
+        ;
+
+    }
     private function getOrCreateQueryBuilder( QueryBuilder $qb=null)
     {
         return $qb ? : $this->createQueryBuilder('a');
