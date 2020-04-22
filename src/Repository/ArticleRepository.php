fc99f5cd5abfd6eb052d531164b3183027c02552
Swati Joshi <swatijoshi@outlook.de>
2020-04-22 15:24:46 +0200

    optimising query

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -29,6 +29,8 @@ class ArticleRepository extends ServiceEntityRepository
     {
         
         return $this->addIsPublishedQueryBuilder()
+            ->leftJoin('a.tags','t')
+            ->addSelect('t')
             ->orderBy('a.publishedAt', 'DESC')
             ->getQuery()
             ->getResult()
