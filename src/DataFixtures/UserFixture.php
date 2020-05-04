50fd740b24ec66f98d2e94d5a0d57f581006bf5d
Swati Joshi <swatijoshi@outlook.de>
2020-05-04 11:32:23 +0200

    added new password column and run fixtures to save encoded password for all users

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -4,10 +4,15 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Common\Persistence\ObjectManager;
-
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixture extends BaseFixture
 {
+    private $passwordEncoder;
+    public function __construct( UserPasswordEncoderInterface $passwordEncoder )
+    {
+        $this->passwordEncoder=$passwordEncoder;
+    }
     protected function loadData(ObjectManager $manager)
     {
         $this->createMany(10, 'main_user',function($i){
@@ -15,7 +20,10 @@ class UserFixture extends BaseFixture
             $user=new User;
             $user->setEmail(sprintf('spacebar%d@example.com',$i));
             $user->setFirstName($this->faker->firstName);
-
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'engage'
+            ));
             return $user;
         });
 
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -31,6 +31,11 @@ class User implements UserInterface
      * @ORM\Column(type="string", length=255)
      */
     private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
     public function getId(): ?int
     {
         return $this->id;
@@ -82,7 +87,7 @@ class User implements UserInterface
      */
     public function getPassword()
     {
-        // not needed for apps that do not check user passwords
+        return $this->password;
     }
 
     /**
@@ -110,6 +115,13 @@ class User implements UserInterface
     public function setFirstName(string $firstName): self
     {
         $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
 
         return $this;
     }
