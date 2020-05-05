3fd32e79c1e8776823f353afeee6062853d3df94
Swati Joshi <swatijoshi@outlook.de>
2020-05-05 17:24:16 +0200

    Adding fixtures for api token and user

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\ApiToken;
 use App\Entity\User;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -15,7 +16,7 @@ class UserFixture extends BaseFixture
     }
     protected function loadData(ObjectManager $manager)
     {
-        $this->createMany(10, 'main_user',function($i){
+        $this->createMany(10, 'main_user',function($i) use($manager){
 
             $user=new User;
             $user->setEmail(sprintf('spacebar%d@example.com',$i));
@@ -27,6 +28,16 @@ class UserFixture extends BaseFixture
                 $user,
                 'engage'
             ));
+
+            //With createMany() method, you do not need to call persist() or flush() 
+            //on the object that you return
+            
+            //if you create some objects manually - like this - you do need to call persist()
+            $apiToken1= new ApiToken($user);
+            $apiToken2= new ApiToken($user);
+            $manager->persist($apiToken1);
+            $manager->persist($apiToken2);
+
             return $user;
         });
 
